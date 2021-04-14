138c2487c4e8cb412673860cd4c8a2c9781b5b11
erego <eladiorego@yahoo.es>
2021-04-14 12:57:58 +0200

    Creating abt with pandas format

--- Predilectura/routes.py
+++ Predilectura/routes.py
@@ -1,6 +1,12 @@
 """Routes for parent Flask app."""
+
+from pathlib import Path
+
 from flask import current_app as app
 from flask import render_template, request
+
+import pandas as pd
+
 from Predilectura import mongo
 from Predilectura.data.abt import ABTMongoDB, ABTPandas
 
@@ -65,96 +71,114 @@ def lista_readings():
                            next=current_page+1, total_pages=total_pages)
 
 
-@app.route("/listar_ABT")
-def lista_ABT():
+@app.route("/listar_ABT", defaults={'format_data': "mongodb"})
+@app.route('/listar_ABT/<format_data>')
+def lista_ABT(format_data):
 
     """
     Page which list reading collection from db
     """
 
-    total_records = mongo.db.abt.count()
     number_of_records = 10
-    total_pages = int(total_records / 10) + 1
-    current_page = int(request.args.get('page', default=1))
-    number_to_skip = number_of_records * (current_page-1)
 
-    # get headers
-    dict_header = dict()
-    dict_header["user_id"] = True
-    dict_header["book"] = True
-    dict_header["language"] = True
-    dict_header["min_percent"] = False
-    dict_header["max_percent"] = False
-    dict_header["avg_percent"] = False
-    dict_header["max_words"] = False
-    dict_header["min_words"] = False
-    dict_header["avg_words"] = False
-    dict_header["premium"] = False
-    dict_header["devices_readings"] = False
-    dict_header["versions_readings"] = False
-    dict_header["chapters_readings"] = False
-    dict_header["event_classes"] = False
-    dict_header["event_objs"] = False
-    dict_header["event_types"] = False
-    dict_header["devices_events"] = False
-    dict_header["versions_events"] = False
-    dict_header["chapters_events"] = False
+    if format_data == "mongodb":
+        total_records = mongo.db.abt.count()
+        total_pages = int(total_records / 10) + 1
+        current_page = int(request.args.get('page', default=1))
+        number_to_skip = number_of_records * (current_page-1)
 
-    firstElement = mongo.db.abt.find_one()
+        # get headers
+        dict_header = dict()
+        dict_header["user_id"] = True
+        dict_header["book"] = True
+        dict_header["language"] = True
+        dict_header["min_percent"] = False
+        dict_header["max_percent"] = False
+        dict_header["avg_percent"] = False
+        dict_header["max_words"] = False
+        dict_header["min_words"] = False
+        dict_header["avg_words"] = False
+        dict_header["premium"] = False
+        dict_header["devices_readings"] = False
+        dict_header["versions_readings"] = False
+        dict_header["chapters_readings"] = False
+        dict_header["event_classes"] = False
+        dict_header["event_objs"] = False
+        dict_header["event_types"] = False
+        dict_header["devices_events"] = False
+        dict_header["versions_events"] = False
+        dict_header["chapters_events"] = False
 
-    if "min_percent" in firstElement:
-        dict_header["min_percent"] = True
+        firstElement = mongo.db.abt.find_one()
 
-    if "max_percent" in firstElement:
-        dict_header["max_percent"] = True
+        if "min_percent" in firstElement:
+            dict_header["min_percent"] = True
 
-    if "avg_percent" in firstElement:
-        dict_header["avg_percent"] = True
+        if "max_percent" in firstElement:
+            dict_header["max_percent"] = True
 
-    if "max_words" in firstElement:
-        dict_header["max_words"] = True
+        if "avg_percent" in firstElement:
+            dict_header["avg_percent"] = True
 
-    if "min_words" in firstElement:
-        dict_header["min_words"] = True
+        if "max_words" in firstElement:
+            dict_header["max_words"] = True
 
-    if "avg_words" in firstElement:
-        dict_header["avg_words"] = True
+        if "min_words" in firstElement:
+            dict_header["min_words"] = True
 
-    if "premium" in firstElement:
-        dict_header["premium"] = True
+        if "avg_words" in firstElement:
+            dict_header["avg_words"] = True
 
-    if "devices_readings" in firstElement:
-        dict_header["devices_readings"] = True
+        if "premium" in firstElement:
+            dict_header["premium"] = True
 
-    if "versions_readings" in firstElement:
-        dict_header["versions_readings"] = True
+        if "devices_readings" in firstElement:
+            dict_header["devices_readings"] = True
 
-    if "chapters_readings" in firstElement:
-        dict_header["chapters_readings"] = True
+        if "versions_readings" in firstElement:
+            dict_header["versions_readings"] = True
 
-    if "event_classes" in firstElement:
-        dict_header["event_classes"] = True
+        if "chapters_readings" in firstElement:
+            dict_header["chapters_readings"] = True
 
-    if "event_objs" in firstElement:
-        dict_header["event_objs"] = True
+        if "event_classes" in firstElement:
+            dict_header["event_classes"] = True
 
-    if "event_types" in firstElement:
-        dict_header["event_types"] = True
+        if "event_objs" in firstElement:
+            dict_header["event_objs"] = True
 
-    if "devices_events" in firstElement:
-        dict_header["devices_events"] = True
+        if "event_types" in firstElement:
+            dict_header["event_types"] = True
 
-    if "versions_events" in firstElement:
-        dict_header["versions_events"] = True
+        if "devices_events" in firstElement:
+            dict_header["devices_events"] = True
 
-    if "chapters_events" in firstElement:
-        dict_header["chapters_events"] = True
+        if "versions_events" in firstElement:
+            dict_header["versions_events"] = True
 
-    # convert the mongodb object to a list
-    data = list(mongo.db.abt.find().skip(number_to_skip).limit(number_of_records))
+        if "chapters_events" in firstElement:
+            dict_header["chapters_events"] = True
+
+        # convert the mongodb object to a list
+        data = list(mongo.db.abt.find().skip(number_to_skip).limit(number_of_records))
+
+    elif format_data == "pandas":
+        path_to_data = Path(app.root_path).joinpath("data", "abt.csv")
+
+        data = pd.read_csv(path_to_data.as_posix())
+
+        total_records = data.shape[0]
+        total_pages = int(total_records / 10) + 1
+        current_page = int(request.args.get('page', default=1))
+        number_to_skip = number_of_records * (current_page - 1)
+        data = data[number_to_skip:number_to_skip + number_of_records]
+        lst_columns = list(data.columns)
+        dict_header = dict.fromkeys(lst_columns, True)
+
+        data = data.to_dict('records')
 
     return render_template('lista_abt.jinja2', abt=data, prev=current_page-1, current=current_page,
-                           next=current_page+1, total_pages=total_pages, header=dict_header)
+                           next=current_page+1, total_pages=total_pages, header=dict_header, format_data=format_data)
 
 
 @app.route("/generar_abt")
