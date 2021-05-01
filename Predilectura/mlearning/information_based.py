7c86390422fcc226714d1d9afcc2be5763c745cc
erego <eladiorego@yahoo.es>
2021-05-01 10:36:46 +0200

    C45 algorithm

--- Predilectura/mlearning/information_based.py
+++ Predilectura/mlearning/information_based.py
@@ -1,14 +1,14 @@
 """
 Class to define machine learning models related to information based learning
 """
-
-
+import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
 
+from chefboost import Chefboost as chef
+
 
 class CARTAlgorithm:
-
     """
     Class which represents the CART Algorithm(Classification and Regression Trees) for Decision Trees
     in order goal to to create a model that predicts the value of a target variable by learning simple decision rules
@@ -38,15 +38,14 @@ class CARTAlgorithm:
         self.model.fit(self.data_train, self.target_train)
 
     def get_statistical_metrics(self):
-
         predictions = self.get_predictions(self.data_test)
         tn, fp, fn, tp = confusion_matrix(self.target_test, predictions).ravel()
-        accuracy = (tp + tn)/(tn + fn + tp + fp)
-        recall = tp/(tp+fn)
-        specificity = tn/(tn+fp)
-        precision = tp/(tp+fp)
+        accuracy = (tp + tn) / (tn + fn + tp + fp)
+        recall = tp / (tp + fn)
+        specificity = tn / (tn + fp)
+        precision = tp / (tp + fp)
         f1_score = 2 * (recall * precision) / (recall + precision)
-        dict_metrics = {"accuracy": accuracy, "recall": recall, "specificity":specificity,
+        dict_metrics = {"accuracy": accuracy, "recall": recall, "specificity": specificity,
                         "precision": precision, "f1_score": f1_score}
         return dict_metrics
 
@@ -58,24 +57,72 @@ class CARTAlgorithm:
         results = self.model.predict(data_to_predict)
         return results
 
-class C4dot5Algorith:
+
+class C4dot5Algorithm:
     """
        Class which represents the C4.5 Algorithm(Classification and Regression Trees) for Decision Trees
        in order goal to to create a model that predicts the value of a target variable by learning simple decision rules
        inferred from the data features
        """
 
-    def __init__(self, data_train, target_train, data_test, target_test, impurity_metric):
+    def __init__(self, data_train, target_train, data_test, target_test):
         """
 
         :param data_train: features data used to train
         :param target_train: target data used to train
         :param data_test: features data used to tes
         :param target_test: target data used to test
-        :param impurity_metric: type of impurity metric used for the algorithm (entropy, gini)
         """
         self.model = None
         self.data_train = data_train
         self.target_train = target_train
         self.data_test = data_test
         self.target_test = target_test
+        self.config = {'algorithm': 'C4.5'}
+
+    def build_model(self):
+        """
+        Build a decision tree classifier from the training set
+        :return: None, model attribute is updated according to data
+        """
+
+        df_to_build = pd.concat([self.data_train, self.target_train], axis=1)
+
+        self.model = chef.fit(df_to_build, config=self.config, validation_df=pd.concat([self.data_test, self.target_test], axis=1))
+
+    def get_predictions(self, data_to_predict):
+        """
+        Get prediction
+        :return:
+        """
+        results = []
+        for index, row in data_to_predict.iterrows():
+            prediction = chef.predict(self.model, param=row)
+            actual = row["Decision"]
+            results.append((prediction, actual))
+
+        return results
+
+    def get_statistical_metrics(self):
+        df_statistics = pd.concat([self.data_test, self.target_test], axis=1)
+        predictions = self.get_predictions(df_statistics)
+        tp, tn, fp, fn = 0, 0, 0, 0
+
+        for prediction in predictions:
+            if (prediction[0] == prediction[1]) and prediction[0] == "Yes":
+                tp += 1
+            elif (prediction[0] == prediction[1]) and prediction[0] == "No":
+                tn += 1
+            elif (prediction[0] != prediction[1]) and prediction[0] == "Yes":
+                fp += 1
+            elif (prediction[0] != prediction[1]) and prediction[0] == "No":
+                fn += 1
+
+        accuracy = (tp + tn) / (tn + fn + tp + fp)
+        recall = tp / (tp + fn)
+        specificity = tn / (tn + fp)
+        precision = tp / (tp + fp)
+        f1_score = 2 * (recall * precision) / (recall + precision)
+        dict_metrics = {"accuracy": accuracy, "recall": recall, "specificity": specificity,
+                        "precision": precision, "f1_score": f1_score}
+        return dict_metrics
--- Predilectura/test/test_algorithm_information.py
+++ Predilectura/test/test_algorithm_information.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import pandas as pd
 
-from Predilectura.mlearning.information_based import CARTAlgorithm
+from Predilectura.mlearning.information_based import CARTAlgorithm, C4dot5Algorithm
 
 
 class TestTreeAlgortihm(TestCase):
@@ -25,6 +25,35 @@ class TestTreeAlgortihm(TestCase):
         self.x_test = self.data_test[['col1', 'col2']]
         self.y_test = self.data_test['target']
 
+        self.data_train_c45 = pd.DataFrame([{'Outlook': 'Sunny', 'Temperature': 85, 'Humidity':85, 'Wind':'Weak',
+                                            'Decision': 'No'},
+                                           {'Outlook': 'Sunny', 'Temperature': 80, 'Humidity': 90, 'Wind': 'Strong',
+                                            'Decision': 'No'},
+                                           {'Outlook': 'Overcast', 'Temperature': 83, 'Humidity': 78, 'Wind': 'Weak',
+                                            'Decision': 'Yes'},
+                                           {'Outlook': 'Rain', 'Temperature': 70, 'Humidity': 96, 'Wind': 'Weak',
+                                            'Decision': 'Yes'},
+                                           {'Outlook': 'Rain', 'Temperature': 68, 'Humidity': 80, 'Wind': 'Weak',
+                                            'Decision': 'Yes'},
+                                           {'Outlook': 'Rain', 'Temperature': 65, 'Humidity': 70, 'Wind': 'Strong',
+                                            'Decision': 'No'},
+                                           {'Outlook': 'Rain', 'Temperature': 64, 'Humidity': 65, 'Wind': 'Strong',
+                                            'Decision': 'Yes'}])
+
+        self.data_test_c45 = pd.DataFrame([{'Outlook': 'Sunny', 'Temperature': 72, 'Humidity':95, 'Wind':'Weak',
+                                            'Decision': 'No'},
+                                           {'Outlook': 'Sunny', 'Temperature': 69, 'Humidity': 70, 'Wind': 'Weak',
+                                            'Decision': 'Yes'},
+                                           {'Outlook': 'Rain', 'Temperature': 75, 'Humidity': 80, 'Wind': 'Weak',
+                                            'Decision': 'Yes'}])
+
+
+
+        self.x_train_c45 = self.data_train_c45[['Outlook', 'Temperature', 'Humidity', 'Wind']]
+        self.y_train_c45 = self.data_train_c45['Decision']
+        self.x_test_c45 = self.data_test_c45[['Outlook', 'Temperature', 'Humidity', 'Wind']]
+        self.y_test_c45 = self.data_test_c45['Decision']
+
     def test_cart(self):
         cart_model = CARTAlgorithm(self.x_train, self.y_train, self.x_test, self.y_test,
                                    "entropy")
@@ -35,4 +64,15 @@ class TestTreeAlgortihm(TestCase):
         self.assertEqual(statistical_measures["accuracy"], 0.5)
 
 
+    def test_c4dot5(self):
+
+
+        c4dot5_model = C4dot5Algorithm(self.x_train_c45, self.y_train_c45, self.x_test_c45, self.y_test_c45)
+        c4dot5_model.build_model()
+
+        df_to_predict = pd.concat([self.x_test_c45, self.y_test_c45], axis=1)
+        predict = c4dot5_model.get_predictions(df_to_predict)
+        self.assertEqual(predict[0][0], "No")
+        statistical_measures = c4dot5_model.get_statistical_metrics()
+        self.assertEqual(statistical_measures["recall"], 0.5)
 
