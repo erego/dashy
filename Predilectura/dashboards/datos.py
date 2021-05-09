c4cfbfb7ec8750ddb77fed39e548ae11001ba0e3
erego <eladiorego@yahoo.es>
2021-05-09 18:37:26 +0200

    Adding graphs to dash

--- Predilectura/dashboards/datos.py
+++ Predilectura/dashboards/datos.py
@@ -6,12 +6,12 @@ import dash_bootstrap_components as dbc
 import dash_table
 from dash.dependencies import Input, Output
 from flask import url_for
+
 import plotly.express as px
-
-
-from .data import create_dataframe
+from .data import create_dataframe, get_datasets, get_json_algorithm
 
 from .layout import html_layout
+
 
 def init_dashboard(server):
     """Create a Plotly Dash dashboard."""
@@ -24,19 +24,20 @@ def init_dashboard(server):
         ],
     )
 
+    lst_dataset = get_datasets()
+
     df = create_dataframe()
 
     # Custom HTML layout
-    #dash_app.index_string = html_layout
-
+    # dash_app.index_string = html_layout
 
     dash_app.layout = html.Div(children=[
         dbc.Row(
             dbc.NavbarSimple(
                 children=[
-                    html.Img(src=dash_app.get_asset_url('quantified.png') ,height="50",
+                    html.Img(src=dash_app.get_asset_url('quantified.png'), height="50",
                              alt="Quantified Reading logo"),
-                    html.Img(style={"margin-left": "15px"},src=dash_app.get_asset_url('logo_uned.gif') , height="50",
+                    html.Img(style={"margin-left": "15px"}, src=dash_app.get_asset_url('logo_uned.gif'), height="50",
                              alt="UNED logo"),
                 ],
                 brand_href="#",
@@ -47,17 +48,54 @@ def init_dashboard(server):
 
         dbc.Row(
             [dbc.Col(children=
-                [html.Label('Select Feature',style = {"margin-left": "15px"}),
-                dcc.Dropdown(
-                    id="select_feature",
-                    style={"margin-left": "15px"},
-                    options=[
-                        {'label': column, 'value': column} for column in df.columns
-                    ],
-                )]
-            , width=2),
+                     [html.Label('Select Dataset', style={"margin-left": "15px"}),
+                      dcc.Dropdown(
+                          id="select_dataset",
+                          style={"margin-left": "15px"},
+                          options=[
+                              {'label': element, 'value': element} for element in lst_dataset
+                          ],
+                      )]
+                     , width=2),
+
+             ]
+        ),
+
+        dbc.Row(
+            [dbc.Col(children=
+                     [html.Label('Select Feature', style={"margin-left": "15px"}),
+                      dcc.Dropdown(
+                          id="select_feature",
+                          style={"margin-left": "15px"},
+                          options=[
+                              {'label': column, 'value': column} for column in df.columns
+                          ],
+                      )]
+                     , width=2),
              dbc.Col(
                  html.Div(children=html.Div(id='graph_feature')),
+             )
+             ]
+        ),
+
+        dbc.Row(
+            [dbc.Col(children=
+                     [html.Label('Select Algorithm', style={"margin-left": "15px"}),
+                      dcc.Dropdown(
+                          id="select_algorithm",
+                          style={"margin-left": "15px"},
+                          options=[
+                              {'label': 'CART', 'value': 'CART'},
+                              {'label': 'C4.5', 'value': 'C4dot5'},
+                              {'label': 'KMeans', 'value': 'kmeans'},
+                              {'label': 'KNearestNeighbours', 'value': 'KNN'},
+                              {'label': 'Naive Bayes', 'value': 'naivebayes'},
+                              {'label': 'Multilayer Perceptron', 'value': 'mlp'}
+                          ]
+                      )]
+                     , width=2),
+             dbc.Col(
+                 html.Div(children=html.Div(id='graph_algorithm')),
              )
              ]
         ),
@@ -65,36 +103,42 @@ def init_dashboard(server):
         # create_data_table(df)
     ])
 
-    # # Create Layout
-    # dash_app.layout = html.Div(
-    #     children=[
-    #     html.Label('Select Feature'),
-    #     dcc.Dropdown(
-    #         id="select_feature",
-    #         options=[
-    #             {'label': column, 'value': column} for column in df.columns
-    #         ],
-    #     ),
-    #     html.Div(children=html.Div(id='graph_feature'), className='row'),
-    #
-    #         create_data_table(df),
-    #     ],
-    #     id="dash-container",
-    # )
 
     ########## Define Callback
     @dash_app.callback(Output('graph_feature', 'children'),
                        [Input('select_feature', 'value')])
     def update_graph_feature(feature_selected):
         fig = px.histogram(df, x=feature_selected,
-                   title=f'Histogram of {feature_selected}'
-                   )
+                           title=f'Histogram of {feature_selected}'
+                           )
 
         return dcc.Graph(
             id="graph_histogram",
             figure=fig
 
         )
+
+    @dash_app.callback(Output('graph_algorithm', 'children'),
+                       [Input('select_algorithm', 'value'), Input('select_dataset', 'value')])
+    def update_graph_algorithm(select_algorithm, select_dataset):
+
+        if select_algorithm is not None and select_dataset is not None:
+            data = get_json_algorithm(select_algorithm, select_dataset)
+
+            table = dash_table.DataTable(
+                id="database-algorithm",
+                columns=[{"name": i, "id": i} for i in data.keys()],
+                data=data,
+                sort_action="native",
+                sort_mode="native",
+                page_size=300,
+            )
+
+            return table
+
+
+
+
     return dash_app.server
 
 
